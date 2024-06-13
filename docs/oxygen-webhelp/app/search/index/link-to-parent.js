34d695e504a6cf9d3d330be10f25a63b7d16a1a6
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2024-06-13 15:18:53 +0530

    [DOCS] Updated documentation
    MH3-95979

---
+++ docs/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,4 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+define(function () {
+return {1:9,19:9,16:9,7:-1,14:-1,23:3,8:-1,12:9,0:3,15:-1,18:9,4:3,2:3,9:-1,5:9,25:3,3:-1,22:9,6:3,17:3,21:9,26:9,20:3,10:9,13:3,24:9,11:9};
+});
