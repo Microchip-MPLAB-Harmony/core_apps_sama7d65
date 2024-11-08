a8a9fddc203ef2018073cdd1bc4b8d88d1fef394
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2024-11-08 12:34:52 +0530

    [DRV_] Regenerated demo for driver__i2c__sync__i2c_eeprom__sam_a7d65_curiosity_freertos.X
    MH3-104164

--- apps/driver/i2c/sync/i2c_eeprom/src/config/default/definitions.h
+++ apps/driver/i2c/sync/i2c_eeprom/src/config/default/definitions.h
@@ -58,11 +58,11 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include "bsp/bsp.h"
+#include "driver/i2c/drv_i2c.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
-#include "driver/i2c/drv_i2c.h"
 #include "app_i2c_eeprom.h"
 
 
@@ -76,13 +76,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "SAMA7D65"
-#define DEVICE_ARCH			 "CORTEX-A7"
-#define DEVICE_FAMILY		 "SAMA7"
-#define DEVICE_SERIES		 "SAMA7D6"
+#define DEVICE_NAME          "SAMA7D65"
+#define DEVICE_ARCH          "CORTEX-A7"
+#define DEVICE_FAMILY        "SAMA7"
+#define DEVICE_SERIES        "SAMA7D6"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 800000000
+#define CPU_CLOCK_FREQUENCY 800000000U
 
 // *****************************************************************************
 // *****************************************************************************
