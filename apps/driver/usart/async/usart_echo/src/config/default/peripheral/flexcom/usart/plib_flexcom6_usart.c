7cff0e566a671b6a7e1da6dce266f8afdb175a5e
Syam Nair <Syam.Nair@microchip.com>
2025-09-23 09:26:18 +0000

    [APP] Regenerated demo for driver__usart__async__usart_echo
    MH3C-339

--- apps/driver/usart/async/usart_echo/src/config/default/peripheral/flexcom/usart/plib_flexcom6_usart.c
+++ apps/driver/usart/async/usart_echo/src/config/default/peripheral/flexcom/usart/plib_flexcom6_usart.c
@@ -66,9 +66,9 @@
 // Section: FLEXCOM6 USART Implementation
 // *****************************************************************************
 // *****************************************************************************
-volatile static FLEXCOM_USART_OBJECT flexcom6UsartObj;
+static volatile FLEXCOM_USART_OBJECT flexcom6UsartObj;
 
-void static FLEXCOM6_USART_ErrorClear( void )
+static void FLEXCOM6_USART_ErrorClear( void )
 {
     if ((FLEXCOM6_REGS->FLEX_US_CSR & (FLEX_US_CSR_OVRE_Msk | FLEX_US_CSR_FRAME_Msk | FLEX_US_CSR_PARE_Msk)) != 0U)
     {
@@ -91,7 +91,7 @@ void static FLEXCOM6_USART_ErrorClear( void )
 }
 
 
-void static __attribute__((used)) FLEXCOM6_USART_ISR_RX_Handler( void )
+static void __attribute__((used)) FLEXCOM6_USART_ISR_RX_Handler( void )
 {
     uint32_t rxPending = 0;
     uint32_t rxThreshold = 0;
@@ -149,7 +149,7 @@ void static __attribute__((used)) FLEXCOM6_USART_ISR_RX_Handler( void )
     }
 }
 
-void static __attribute__((used)) FLEXCOM6_USART_ISR_TX_Handler( void )
+static void __attribute__((used)) FLEXCOM6_USART_ISR_TX_Handler( void )
 {
     if(flexcom6UsartObj.txBusyStatus == true)
     {
@@ -371,7 +371,7 @@ bool FLEXCOM6_USART_SerialSetup( FLEXCOM_USART_SERIAL_SETUP *setup, uint32_t src
         else
         {
             /* Requested baud can be generated with either with 8x oversampling or with 16x oversampling. Select valid one. */
-            if ((cd1 > 0U )&& (cd1 <= 65535U))
+            if ((cd1 > 0U) && (cd1 <= 65535U))
             {
                 cd0 = cd1;
                 fp0 = fp1;
@@ -542,4 +542,6 @@ bool FLEXCOM6_USART_TransmitComplete( void )
 {
     return ((FLEXCOM6_REGS->FLEX_US_CSR & FLEX_US_CSR_TXEMPTY_Msk) != 0U);
 }
+
+
 
